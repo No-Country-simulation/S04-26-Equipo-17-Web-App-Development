9afbaf63f7636213a9692630e5dec705ef18ae09
Desi Sussu <desiremrv@gmail.com>
2026-05-12 11:44:31 +0200

    refactor: update ProtectedRoute component to use props and improve structure

--- front/src/components/ProtectedRoute.tsx
+++ front/src/components/ProtectedRoute.tsx
@@ -1,6 +1,17 @@
-export default function protectedRoute(nextpage: any, requiredStep: number) {
-  return (
-    <div>protectedRoute</div>
-  )
+import { Outlet } from "react-router-dom";
+
+interface ProtectedRouteProps {
+  nextpage?: string;
+  requiredStep?: number;
 }
 
+export default function ProtectedRoute({
+  nextpage,
+  requiredStep,
+}: ProtectedRouteProps) {
+  // TODO: implementar lógica de protección de rutas
+  void nextpage;
+  void requiredStep;
+
+  return <Outlet />;
+}
