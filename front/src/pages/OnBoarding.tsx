f1a072b0cddfd80ee84752e1ed8a1df2bc7d3a05
Desire M <desire.maya@sngular.com>
2026-05-18 09:26:39 +0200

    ui: button

--- front/src/pages/OnBoarding.tsx
+++ front/src/pages/OnBoarding.tsx
@@ -1,11 +1,12 @@
+import { Button } from '@/components/ui/button';
 import { useNavigate } from 'react-router-dom'
 
 export default function OnBoarding() {
-        const navigate = useNavigate();
-        return (
-            <>
-                <div>onBoarding</div>
-                <button onClick={() => navigate('/personalData')}>Empezar OnBoarding</button>
-            </>
-        )
+    const navigate = useNavigate();
+    return (
+        <>
+            <div>onBoarding</div>
+            <Button variant={"default"} size={"sm"} onClick={() => navigate('/personalData')}>Empezar OnBoarding</Button>
+        </>
+    )
 }
